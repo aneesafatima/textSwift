9b1ee4a7f4c5176272e1421f90963849988465a0
aneesa04 <aneesafatima238@gmail.com>
2024-05-10 16:46:52 +0530

    worked on the toolbar component

--- src/components/ToolBar.jsx
+++ src/components/ToolBar.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
-
 function ToolBar() {
   return (
     <div className='toolbar'>
-      Hello from Toolbar
+      <i className="fa-solid fa-bold toolbar-icons"></i>
+      <i class="fa-solid fa-italic toolbar-icons"></i>
+      <i class="fa-solid fa-underline toolbar-icons"></i>
+      <i class="fa-solid fa-list toolbar-icons"></i>
+      <i class="fa-solid fa-list-ol toolbar-icons"></i>
+      <i class="fa-solid fa-align-left toolbar-icons"></i>
+      <i class="fa-solid fa-align-justify toolbar-icons"></i>
+      <i class="fa-solid fa-align-center toolbar-icons"></i>
+      <i class="fa-solid fa-droplet toolbar-icons"></i>
+      <i class="fa-solid fa-fill toolbar-icons"></i>
+      <div className='dropdown'>
+        <button className='dropdown-toggle'>Normal<i class="fa-solid fa-angle-down fa-2xs" style={{margin: "3px"}}></i></button>
+        <ul className='dropdown-list'>
+        <li>roboto</li>
+        <li>mullis</li>
+        <li>sans</li>
+        
+        </ul>
+      </div>
     </div>
   )
 }
