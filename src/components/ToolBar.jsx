724758baf7a118cb280c3175f11c6dcc8c915093
aneesa04 <aneesafatima238@gmail.com>
2024-05-12 21:23:12 +0530

    designed toolbar

--- src/components/ToolBar.jsx
+++ src/components/ToolBar.jsx
@@ -1,14 +1,34 @@
-import React, { useEffect } from 'react'
-import { Fonts } from '.'
+import React, { useEffect, useContext } from 'react';
+import { globalState } from '../context/globalState';
+import { Fonts, TextSize } from '.';
 function ToolBar() {
-  const showOptions = (e) =>{
-    const dropdownList = document.querySelector(".dropdown-list");
-    dropdownList.style.display = "block";
-    
+  const MyContext = useContext(globalState);
+  const {content, setContent} = MyContext;
+
+  //REFACTOR THESE FUNCTIONS INTO 1
+
+  const showFontsOptions = (e) =>{
+    const fontsList = document.querySelector(".dropdown-fonts");
+    const dropdownList = fontsList.querySelector(".dropdown-list");
+    const dropdownListArrow = fontsList.querySelector(".fa-angle-down");
+    dropdownList.classList.toggle("dropdown-list-show");
+    dropdownListArrow.classList.toggle("fa-xmark")
   }
+  const showNumberOptions = (e) =>{
+    const textSizeList = document.querySelector(".dropdown-number")
+    const dropdownList = textSizeList.querySelector(".dropdown-list");
+    const dropdownListArrow = textSizeList.querySelector(".fa-angle-down");
+    dropdownList.classList.toggle("dropdown-list-show");
+    dropdownListArrow.classList.toggle("fa-xmark")
+  }
+
+  const handleBoldClick = (e) =>{
+
+  }
+
   return (
     <div className='toolbar'>
-      <i className="fa-solid fa-bold toolbar-icons"></i>
+      <i className="fa-solid fa-bold toolbar-icons" onClick={handleBoldClick}></i>
       <i class="fa-solid fa-italic toolbar-icons"></i>
       <i class="fa-solid fa-underline toolbar-icons"></i>
       <i class="fa-solid fa-list toolbar-icons"></i>
@@ -18,10 +38,18 @@ function ToolBar() {
       <i class="fa-solid fa-align-center toolbar-icons"></i>
       <i class="fa-solid fa-droplet toolbar-icons"></i>
       <i class="fa-solid fa-fill toolbar-icons"></i>
-      <div className='dropdown'>
-        <button className='dropdown-toggle'> select font <i className="fa-solid fa-angle-down fa-2xs" onClick={showOptions} style={{margin: "3px"}}></i></button>
+      <div className='dropdown-fonts'>
+        <button className='dropdown-toggle'> font family <i className="fa-solid fa-angle-down fa-2xs drop-arrow" onClick={showFontsOptions} style={{margin: "3px"}}></i></button>
        <Fonts/>
       </div>
+      <div className='dropdown-number'>
+      <button className='dropdown-toggle'> text size <i className="fa-solid fa-angle-down fa-2xs drop-arrow" onClick={showNumberOptions} style={{margin: "3px"}}></i></button>
+         <TextSize/>
+      </div>
+      <div className='features'>
+       FEATURES
+      </div>
+     
     </div>
   )
 }
