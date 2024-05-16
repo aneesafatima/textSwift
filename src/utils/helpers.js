587bee1a455c89b849e8654b843c08a7dd365a53
aneesa04 <aneesafatima238@gmail.com>
2024-05-16 19:30:10 +0530

    added text to speech functionality

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,3 +1,14 @@
+let SpeechRecognition;
+
+window.addEventListener("DOMContentLoaded", () => {
+   SpeechRecognition =
+  window.SpeechRecognition || window.webkitSpeechRecognition;
+const SpeechGrammarList =
+  window.SpeechGrammarList || window.webkitSpeechGrammarList;
+const SpeechRecognitionEvent =
+  window.SpeechRecognitionEvent || window.webkitSpeechRecognitionEvent;
+})
+
 export const toggleDarkMode = () =>{
     const body = document.body;
     body.classList.toggle("dark-mode");
@@ -12,6 +23,33 @@ export const allFontsLink = (arr) =>{
  linkElement.type = "text/css"
  linkElement.rel = "stylesheet"
  document.head.appendChild(linkElement);
-
-console.log(linkURL)
 }
+
+
+export const handleBoldClick =  () =>{
+
+}
+
+export const handleSpeechToText = (isSpeaking, setCurrentText) => {
+console.log("Start saying something ! ");
+let generatedText;
+const recognition = new SpeechRecognition();
+recognition.continuous = true;
+recognition.lang = "en-US";
+recognition.onresult = function(event){
+  console.log(event.results)
+  for(let i=0; i<event.results?.length;i++)
+    { 
+      generatedText = generatedText + event.results[i][0].transcript +' ';
+    }
+  setCurrentText(generatedText);
+  generatedText="";
+}
+recognition.onerror = function(event) {
+  console.error('Speech recognition error: ', event.error);
+}
+
+!isSpeaking ? recognition.start() :   recognition.stop();
+
+}
+
