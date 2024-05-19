ada8a6e1300cbea65cbf4b0f0fda07524489e041
aneesa04 <aneesafatima238@gmail.com>
2024-05-19 20:22:29 +0530

    fixed the alignment and list features

--- src/components/ToolBar.jsx
+++ src/components/ToolBar.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useContext, useState } from 'react';
 import { globalState } from '../context/globalState';
-import { Fonts, TextSize } from '.';
-import { handleSpeechToText, handleTimerUI, handleSelectedTime } from '../utils/helpers';
+import { Fonts, TextSize, Timer } from '.';
+import { handleSpeechToText } from '../utils/helpers';
 import { handleClick } from '../utils/toolbarFeatures';
 
 function ToolBar() {
@@ -9,7 +9,6 @@ function ToolBar() {
   // const MyContext = useContext(globalState);
 
   const [isSpeaking, setIsSpeaking] = useState(false);
-  const [timerMins, setTimerMins] = useState([25, 45, 60]);
   const [isBold , setIsBold] = useState(false);
   const [isItalic, setIsItalic] = useState(false);
   const [isUnderline , setIsUnderline] = useState(false);
@@ -18,7 +17,7 @@ function ToolBar() {
   const [isLeftAlign, setIsLeftAlign] = useState(false);
   const [isJustifyAlign, setIsJustifyAlign] = useState(false);
   const [isCenterAlign, setIsCenterAlign] = useState(false);
-  const [isRightAlign , setRightAlign] = useState(false);
+  const [isRightAlign , setIsRightAlign] = useState(false);
   
 
   //REFACTOR THESE FUNCTIONS INTO 1
@@ -43,39 +42,31 @@ function ToolBar() {
 
   return (
     <div className='toolbar'>
-      <i className="fa-solid fa-bold toolbar-icons"  onClick={() =>  handleClick(isBold, setIsBold, "bold")}></i>
-      <i class="fa-solid fa-italic toolbar-icons"  onClick={() => handleClick(isItalic, setIsItalic, "italic")}></i>
-      <i class="fa-solid fa-underline toolbar-icons" onClick={() => handleClick(isUnderline, setIsUnderline, "underline")}></i>
-      <i class="fa-solid fa-list toolbar-icons" onClick={() => handleClick(isUnorderedList , setIsUnorderedList)}></i>
-      <i class="fa-solid fa-list-ol toolbar-icons" onClick={() => handleClick(isOrderedList , setIsOrderedList)}></i>
-      <i class="fa-solid fa-align-left toolbar-icons" onClick={() => handleClick(isLeftAlign, setIsLeftAlign, "left-align")}></i>
-      <i class="fa-solid fa-align-justify toolbar-icons" onClick={() => handleClick(isJustifyAlign, setIsJustifyAlign, "")}></i>
-      <i class="fa-solid fa-align-center toolbar-icons" onClick={() => handleClick(isCenterAlign , setIsCenterAlign, "center-align")}></i>
-      <i class="fa-solid fa-droplet toolbar-icons" onClick={() => handleClick()}></i>
+      <i class="fa-regular fa-copy toolbar-icons"></i>
+      <i class="fa-solid fa-trash toolbar-icons"></i>
+      <i className="fa-solid fa-bold toolbar-icons"  onClick={(e) =>  handleClick(e,isBold, setIsBold, "bold")}></i>
+      <i class="fa-solid fa-italic toolbar-icons"  onClick={(e) => handleClick(e,isItalic, setIsItalic, "italic")}></i>
+      <i class="fa-solid fa-underline toolbar-icons" onClick={(e) => handleClick(e,isUnderline, setIsUnderline, "underline")}></i>
+      <i class="fa-solid fa-list toolbar-icons" onClick={(e) => handleClick(e,isUnorderedList , setIsUnorderedList, "unordered-list")}></i>
+      <i class="fa-solid fa-list-ol toolbar-icons" onClick={(e) => handleClick(e,isOrderedList , setIsOrderedList,"ordered-list")}></i>
+      <i class="fa-solid fa-align-left toolbar-icons" onClick={(e) => handleClick(e,isLeftAlign, setIsLeftAlign, "left-align")}></i>
+      <i class="fa-solid fa-align-justify toolbar-icons" onClick={(e) => handleClick(e,isJustifyAlign, setIsJustifyAlign, "justify-align")}></i>
+      <i class="fa-solid fa-align-right toolbar-icons" onClick={(e) => handleClick(e,isRightAlign , setIsRightAlign, "right-align")}></i>
+      <i class="fa-solid fa-align-center toolbar-icons" onClick={(e) => handleClick(e,isCenterAlign , setIsCenterAlign, "center-align")}></i>
+      {/* <input type="color" className=''/> */}
+     <i class="fa-solid fa-droplet toolbar-icons" onClick={() => handleClick()}></i>
       <i class="fa-solid fa-fill toolbar-icons" onClick={() => handleClick()}></i>
-      <div className='dropdown-fonts'>
-        <button className='dropdown-toggle'> font family <i className="fa-solid fa-angle-down fa-2xs drop-arrow" onClick={showFontsOptions} style={{margin: "3px"}}></i></button>
-       <Fonts/>
-      </div>
-      <div className='dropdown-number'>
-      <button className='dropdown-toggle'> text size <i className="fa-solid fa-angle-down fa-2xs drop-arrow" onClick={showNumberOptions} style={{margin: "3px"}}></i></button>
-         <TextSize/>
-      </div>
+      <Fonts/>
+       <TextSize/>
         <i class="fa-solid fa-microphone toolbar-icons" onClick={() => handleSpeechToText(isSpeaking, setIsSpeaking)}></i>
  <div className='timer'>
- 
- <div className='timers-container'>
-  <i class="fa-regular fa-hourglass toolbar-icons" onClick={handleTimerUI}></i>
-  { Array.isArray(timerMins) ? 
-    timerMins.map((el,index) =>(<div onClick={(e) => handleSelectedTime(e,index, setTimerMins, timerMins) } key ={index} >
-    {el}
-    </div>)) : <div className='selected-time'>{timerMins}</div>
-}
- </div>
+<Timer/>
  </div>
     </div>
   )
 }
 
 export default ToolBar
+
+
 
--- src/utils/toolbarFeatures.js
+++ src/utils/toolbarFeatures.js
@@ -1,27 +1,39 @@
-function handleRange(featureMode, updateFeatureMode, featureStyle){
+
+
+function handleRange(e, featureMode, updateFeatureMode, featureStyle){
     const selection = window.getSelection();
     if(selection.rangeCount > 0)
-    {
+    {  const range = selection.getRangeAt(0);
+      const extractContents = range.extractContents();
+      const newEl = document.createElement("span");
+      console.log(selection.rangeCount)
+    
+      if(featureMode === "fontFamily" || featureMode === "fontSize"){
+         extractContents.childNodes.forEach(el => el.style[featureMode] = "");
+         newEl.style[featureMode] = document.querySelector("dropdown-" + featureMode).value;
+         newEl.appendChild(extractContents);
+      }
+     else{
+      e.target.classList.toggle("active");
     updateFeatureMode(!featureMode);
-    const range = selection.getRangeAt(0);
-    const extractContents = range.extractContents();
-    const newEl = document.createElement("span");
+    
     if(featureMode)
        extractContents.querySelectorAll('.' + featureStyle).forEach(el => el.classList.remove(featureStyle))
-    else {
-        //   if(featureStyle.includes("align"))
-        //     newEl.style.display = "block";
-           newEl.classList.add(featureStyle);  
-    }
-
+    else 
+       newEl.classList.add(featureStyle);  
+   
     newEl.appendChild(extractContents);
+     }
+    
     range.insertNode(newEl)
     selection.addRange(range);
 }
 }
 
 
-export const handleClick =  (featureMode, updateFeatureMode, featureStyle) => {
-    handleRange(featureMode, updateFeatureMode, featureStyle)
+export const handleClick =  (e, featureMode, updateFeatureMode, featureStyle) => {
+    handleRange(e,featureMode, updateFeatureMode, featureStyle)
     }
+
+
 
