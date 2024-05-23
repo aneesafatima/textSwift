65d697786016cf56c478955d27d7052dd9537677
aneesa04 <aneesafatima238@gmail.com>
2024-05-23 16:23:15 +0530

    added copy/paste status notification

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React , {useContext, useEffect} from 'react'
 import { Link } from 'react-router-dom'
 import {toggleDarkMode} from "../utils/helpers"
+import {globalState} from "../context/globalState"
+
 function TopBar() {
+  const myContext = useContext(globalState);
+  const {status, setStatus} = myContext;
+  useEffect(()=> {
+    const statusEl = document.querySelector(".status");
+    if(status !== ""){
+    statusEl.style.top = "15px";
+    setTimeout(()=> {
+    statusEl.style.top = "-400px"
+    setStatus("");
+    }, 1500)
+    }
+  
+  }, [status])
+  
   return (
     <div className='top-bar'>
+      <span className='status'>{status}</span>
       <div className='heading'>  
         <h1 className='title'>textswift
         <span>Subtle.Seamless.Craft</span></h1>
